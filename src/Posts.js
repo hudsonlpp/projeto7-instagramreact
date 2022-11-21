4671c672259496ff0d2f08f4d9b033520b26d082
Hudson Peres <hudsonluizperes@poli.ufrj.br>
2022-11-21 16:56:13 -0300

    completo sem bonus

--- src/Posts.js
+++ src/Posts.js
@@ -1,16 +1,22 @@
+import React from "react";
+
 export default function Posts() {
 
-    let posts = [{foto: "assets/img/meowed.svg",perfil: "meowed",conteudo: "assets/img/gato-telefone.svg", curtidas: "Curtido por <strong>respondeai</strong> e <strong>outras 101.523 pessoas</strong>"},
-                {foto: "assets/img/barked.svg",perfil: "barked",conteudo: "assets/img/dog.svg", curtidas: "Curtido por <strong>adorable_animals</strong> e <strong>outras 99.159 pessoas</strong>"},]
-
+    let posts = [{foto: "assets/img/meowed.svg",perfil: "meowed",conteudo: "assets/img/gato-telefone.svg", like: "respondeai", likes: 101523},
+                {foto: "assets/img/barked.svg",perfil: "barked",conteudo: "assets/img/dog.svg", like: "adorable_animals", likes: 99159},]
     return (
         <div class="posts">
-            {posts.map((f) => <GeraPost foto={f.foto} perfil={f.perfil} conteudo={f.conteudo} curtidas={f.curtidas}/>)}
+            {posts.map((f) => <GeraPost foto={f.foto} perfil={f.perfil} conteudo={f.conteudo} like={f.like} likes={f.likes}/>)}
         </div>
     );
   }
 
   function GeraPost(props){
+
+    const[salvar,setSalvar] = React.useState("bookmark-outline")
+    const[curtir,setCurtir] = React.useState("heart-outline")
+    const[umLike,setUmLike] = React.useState(props.likes)
+
     return(
         <div class="post">
           <div class="topo">
@@ -24,28 +30,54 @@ export default function Posts() {
           </div>
 
           <div class="conteudo">
-            <img src={props.conteudo} />
+            <img onDoubleClick={CurteImagem} src={props.conteudo} />
           </div>
 
           <div class="fundo">
             <div class="acoes">
               <div>
-                <ion-icon name="heart-outline"></ion-icon>
+                <ion-icon onClick={Curtida} name={curtir}></ion-icon>
                 <ion-icon name="chatbubble-outline"></ion-icon>
                 <ion-icon name="paper-plane-outline"></ion-icon>
               </div>
               <div>
-                <ion-icon name="bookmark-outline"></ion-icon>
+                <ion-icon onClick={Salvada} name={salvar}></ion-icon>
               </div>
             </div>
 
             <div class="curtidas">
               <img src="assets/img/respondeai.svg" />
               <div class="texto">
-              {props.curtidas}
+              Curtido por <strong>{props.like}</strong> e <strong> outras {umLike} pessoas</strong>.
               </div>
             </div>
           </div>
         </div>
     )
+
+    function Salvada(){
+        if(salvar === "bookmark-outline") {
+            setSalvar("bookmark")
+        } else {
+            setSalvar("bookmark-outline")
+        }
+    }
+
+    function Curtida(){
+        if(curtir === "heart-outline"){
+            setCurtir("heart")
+            setUmLike(props.likes + 1)
+        } else {
+            setCurtir("heart-outline")
+            setUmLike(props.likes - 1)
+        }
+    }
+
+    function CurteImagem(){
+        if(curtir === "heart-outline"){
+            setCurtir("heart")
+            setUmLike(props.likes + 1)
+        }
+    }
+
 }
--- src/Usuario.js
+++ src/Usuario.js
@@ -1,17 +1,35 @@
+import React from "react";
+
 export default function Usuario() {
+    const [perfil,setPerfil] = React.useState("Catana")
+    const [foto,setFoto] = React.useState("assets/img/catanacomics.svg")
 
-    let usuario = {foto:"assets/img/catanacomics.svg", perfil: "Catana"}
+    return(
+        <GeraUsuario usuario={perfil} imagem={foto} />
+    )
 
-    return (
+    function inserirPerfil(){
+        const resposta = prompt("Digite seu nome.")
+        setPerfil(resposta)
+    }
+
+    function inserirFoto(){
+        const resposta = prompt("Insira o link da sua foto.")
+        setFoto(resposta)
+    }
+
+    function GeraUsuario(props){
+        return (
             <div class="usuario">
-              <img src={usuario.foto} />
+              <img onClick={inserirFoto} src={!foto ? "assets/img/catanacomics.svg" : foto} />
               <div class="texto">
                 <strong>catanacomics</strong>
                 <span>
-                  {usuario.perfil}
-                  <ion-icon name="pencil"></ion-icon>
+                {!perfil ? "Catana" : perfil}
+                  <ion-icon onClick={inserirPerfil} name="pencil"></ion-icon>
                 </span>
               </div>
             </div>    
-    );
+        );
+    }
   }
