d21afb85801ae5239c24714fb529af5b970b5778
Hudson Peres <hudsonluizperes@poli.ufrj.br>
2022-11-21 04:21:02 -0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import Navbar from "./Navbar";
+import Corpo from "./Corpo";
+
+export default function App() {
+  return (
+    <div>
+      <Navbar />
+      <Corpo />
+    </div>
+  );
+}
+
+
+//- App
+//- Corpo
+//- SideBar
+//- NavBar
+//- Usuario
+//- Stories (onde cada item será um componente diferente, o Story)
+//- Posts (onde cada item será um componente diferente, o Post)
+//- Sugestoes (onde cada item será um componente diferente, o Sugestao)
